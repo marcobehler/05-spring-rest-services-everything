5d5053fa10a573c901fb7419d2def6cc78e0b566
Marco Behler <marco@marcobehler.com>
2017-12-20 09:46:00 +0100

    sorting

--- src/main/java/com/marcobehler/springbootdemo/service/CustomerService.java
+++ src/main/java/com/marcobehler/springbootdemo/service/CustomerService.java
@@ -1,18 +1,10 @@
 package com.marcobehler.springbootdemo.service;
 
-import com.marcobehler.springbootdemo.controller.ResourceNotFoundException;
 import com.marcobehler.springbootdemo.domain.Customer;
-import com.marcobehler.springbootdemo.domain.Status;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 
-import javax.sql.DataSource;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.ThreadLocalRandom;
 
 /**
  * Thanks for watching this episode! Send any feedback to info@marcobehler.com!
